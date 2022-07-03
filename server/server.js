9c36e73b3921b0412c2f90f663ad7b627b54d045
lopez <jerome_lopez@hotmail.Com>
2022-07-03 14:19:47 +0200

    path server build

--- server/server.js
+++ server/server.js
@@ -31,6 +31,6 @@ if (process.env.NODE_ENV == "production") {
 
   app.get("*", (req, res) => {
     res.set("Access-Control-Allow-Origin", "*");
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "client/build", "index.html"));
   });
 }
